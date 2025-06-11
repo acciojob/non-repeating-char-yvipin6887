3f6771454e1b19e6e2d989b108ccf564c6385a8e
Vipin Yadav <76983380+yvipin6887@users.noreply.github.com>
2025-06-11 23:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let charCount = {};
+	for (let char of str) {
+		charCount[char] = (charCount[char] | 0) + 1 
+	}
+	
+	for (let char of str) {
+	    if (charCount[char] == 1) {
+	        return char;
+	    }
+	}
+	
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
